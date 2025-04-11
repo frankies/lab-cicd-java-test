20bcb8f3c3239811ef911a82f28314b28b373d1a
frankies <linzhw@gmail.com>
2025-04-11 09:09:27 +0000

    add test code

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,9 +1,12 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
 
 public class TestMain {
    
     @Test
     void test()  {
-    
+      fail("Test failed");
     }
     
 }
