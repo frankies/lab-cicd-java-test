10bc84da62f1a1ad9048f634ec209088fca70bce
frankies <linzhw@gmail.com>
2025-05-07 01:51:04 +0000

    sdkman init

--- install-java17.sh
+++ install-java17.sh
@@ -1,4 +1,4 @@
-curl -s "https://get.sdkman.io" | bash && \
+curl -s "https://get.sdkman.io" | bash && source "/usr/local/sdkman/bin/sdkman-init.sh" \
 sdk selfupdate force && \
  sdk install java 17.0.0-tem && \
  sdk use java 17.0.0-tem
